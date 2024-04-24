c0fff5a110ef7b2b723ffcb9d3e9989efc4de1c8
estefany Jasmine Siguantay-Cortez <esiguant@circinus-39.ics.uci.edu>
2024-04-23 18:39:55 -0700

    worker log and server crash handling

--- crawler/worker.py
+++ crawler/worker.py
@@ -18,17 +18,33 @@ class Worker(Thread):
         super().__init__(daemon=True)
         
     def run(self):
+        retry_count = 0
+        count = 0
         while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
-            resp = download(tbd_url, self.config, self.logger)
-            self.logger.info(
-                f"Downloaded {tbd_url}, status <{resp.status}>, "
-                f"using cache {self.config.cache_server}.")
-            scraped_urls = scraper.scraper(tbd_url, resp)
-            for scraped_url in scraped_urls:
-                self.frontier.add_url(scraped_url)
-            self.frontier.mark_url_complete(tbd_url)
-            time.sleep(self.config.time_delay)
+            try:
+                retry_count = 0
+                resp = download(tbd_url, self.config, self.logger)
+                self.logger.info(
+                    f"Downloaded {tbd_url}, status <{resp.status}>, "
+                    f"using cache {self.config.cache_server}.")
+                
+                self.logger.info(f"FRONTIER {self.frontier.to_be_downloaded}")
+                scraped_urls = scraper.scraper(tbd_url, resp)
+                for scraped_url in scraped_urls:
+                    self.frontier.add_url(scraped_url)
+                self.frontier.mark_url_complete(tbd_url)
+                time.sleep(self.config.time_delay)
+            except:
+                retry_count += 1
+                self.frontier.add_url(tbd_url)
+                if retry_count > 3:
+                    skipped = self.frontier.get_tbd_url()
+                    print(f"Exceeded retry amounts for: {skipped}")
+                else:
+                    time.sleep(self.config.time_delay * retry_count)
+            count += 1
+        
